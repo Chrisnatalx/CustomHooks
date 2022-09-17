b77f4c7efb1a7c12e9f29cd65b3c783404e56b63
Chrisnatalx <natale.christian.a@gmail.com>
2022-09-16 22:58:36 -0300

    Primer commit

---
+++ useTodo.js
@@ -0,0 +1,58 @@
+import { todoReducer } from "../08-useReducer/todoReducer"
+import { useEffect, useReducer } from "react"
+export const useTodo = () => {
+    const initialState = [
+        // {
+        //     id: new Date().getTime(),
+        //     descripcion:'Recolectar la piedra del Alma',
+        //     done:false,
+        // },
+    
+    ];
+    const init = () =>{
+        return JSON.parse(localStorage.getItem('todos') || [])
+    }
+
+    const [todos, dispatch] = useReducer(todoReducer, initialState, init)
+
+    useEffect(() => {
+      localStorage.setItem('todos',JSON.stringify(todos))
+    
+    }, [todos])
+    
+
+    const handleNewTodo =(todo) => {
+        const action = {
+            type: '[TODO] Add Todo',
+            payload: todo
+        }
+        dispatch(action)
+    } 
+
+    const handleDeleteTodo = (id) => {
+        dispatch({
+            type: '[TODO] Remove Todo',
+            payload: id
+        })
+
+    }
+    const handleToggleTodo = (id) => {
+        dispatch({
+            type: '[TODO] Toggle Todo',
+            payload: id
+        })
+
+    }
+
+
+  return {
+    ...initialState,
+    todos,
+    todosCount: todos.length,
+    pendingTodosCount:todos.filter( todo => !todo.done).length,
+    handleDeleteTodo,
+    handleNewTodo,
+    handleToggleTodo,
+
+  }
+}
